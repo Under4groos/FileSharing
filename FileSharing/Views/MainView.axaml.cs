90a6a4ec5ff1ef764fdd96102844acf297368922
Макс Титов <hhit934@gmail.com>
2025-05-17 17:27:39 +0300

    Добавьте файлы проекта.

---
+++ FileSharing/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace FileSharing.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ FileSharing/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace FileSharing.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
